fc654066233b96ecbd2cd062c8e211918e02cee6
pyinapl <1003115140@qq.com>
2018-03-20 11:51:12 +0800

    demo

---
+++ src/main/java/com/spring/wvk/WvkApplication.java
@@ -0,0 +1,12 @@
+package com.spring.wvk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WvkApplication.class, args);
+	}
+}
